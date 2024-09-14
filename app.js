1912a1a4409def597f3509b0cd45384c39cbfedb
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2024-09-14 19:14:27 -0300

    add: Checking the program flow using console.log and commenting the code

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 4;
-console.log(numeroSecreto)
-let chute = prompt("Escolha um número entre 1 e ");
+let numeroSecreto = 9;
+console.log(`O numero secreto é ${numeroSecreto}` );
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert("Você errou :(");
+let chute = prompt("Escolha um número entre 1 e 10");
+console.log(`o chute foi ${chute}`);
+
+// Se o chute for igual ao numero secreto:
+if (chute == numeroSecreto){
+    alert(`Isso ai, você descobriu o numero secreto ${numeroSecreto}`);
+    console.log("acertou o numero");
+} 
+// Senão, faça isso:
+else{
+    alert("Você errou ;(");
+    console.log("Errou o numero");
 }
-
+console.log(`o chute foi ${chute} e o numero secreto é ${numeroSecreto}.`);
