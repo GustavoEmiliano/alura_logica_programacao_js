d5e6b5132ed5abf9cc25fbaa776382708deb9912
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2024-09-14 23:59:06 -0300

    add: messages informing if the secret number is higher or lower than the player's guess

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 9;
+let numeroSecreto = 5;
 console.log(`O numero secreto é ${numeroSecreto}` );
 
 let chute = prompt("Escolha um número entre 1 e 10");
@@ -12,7 +12,13 @@ if (chute == numeroSecreto){
 } 
 // Senão, faça isso:
 else{
-    alert("Você errou ;(");
-    console.log("Errou o numero");
+    // Se o chute for maior que o numero secreto exiba que o número secreto é menor que o chute
+    if(chute > numeroSecreto){
+        alert(`O número secreto é menor que ${chute}`);
+    // Senão exiba que o número secreto é maior que o chute
+    } else {
+        alert(`O número secreto é maior que ${chute}`);
+    }
 }
 console.log(`o chute foi ${chute} e o numero secreto é ${numeroSecreto}.`);
+
